91806c3a17a8feedd746c658b05b259a49e0b511
anmolgarg154 <garganmol154@gmail.com>
2025-12-22 11:12:36 +0530

    setup

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,33 +10,21 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 
-connectDb();
-app.get("/", (req, res) => {
-    res.send("Backend is running");
-});
-
-app.get("/api/jokes", (req, res) => {
-    const jokes = [
-        {
-            id: 1,
-            joke: "Why don't scientists trust atoms? Because they make up everything!",
-            content: "Some content here"
-        },
-        {
-            id: 2,
-            joke: "Why did the scarecrow win an award? Because he was outstanding in his field!",
-            content: "Some content here"
-        },
-        {
-            id: 3,
-            joke: "Why did the scarecrow win an award? Because he was outstanding in his field!",
-            content: "Some content here"
-        }
-    ];
-
-    res.json(jokes);
-});
-
-app.listen(port, () => {
+connectDb()
+ .then(()=>{
+    app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+})
+ .catch((err)=>{
+    console.log("Error connecting to MongoDB:", err);
+ })
+
+
+
+
+// app.get("/", (req, res) => {
+//     res.send("Backend is running");
+// });
+
+
