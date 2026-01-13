05398c83d26b372e103178c7d3e97cac542094c2
anmolgarg154 <garganmol154@gmail.com>
2026-01-13 16:11:08 +0530

    validation check in Carrer controller to ensure all fields are required

--- Backend/src/controller/Carrer-controller.js
+++ Backend/src/controller/Carrer-controller.js
@@ -1,6 +1,13 @@
+import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
 
 
 export const Carrer = asyncHandler(async(req,res)=>{
     const {FullName , Email , Phone , Description , Role } = req.body
+
+
+    if(!FullName || !Email || !Phone || !Description || !Role){
+       // throw new ApiError(400, "All fields are required");
+        return res.status(400).json({message: "All fields are required"});
+    }
 })
